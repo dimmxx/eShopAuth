5f4b9a39c1f9216164611f4a0cff2fd9c92d6415
dimmxx <dimmxx@gmail.com>
2019-07-29 16:18:11 +0300

    mac 29-07-2019

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -12,10 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String sha256hex =
-                Hashing.sha256().hashString("qwerty", StandardCharsets.UTF_8).toString();
 
-        System.out.println(sha256hex);
 
 
     }
