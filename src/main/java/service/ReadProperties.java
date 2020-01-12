96fa615da472788caa999e8453f736922c9c96c1
dimmxx <dimmxx@gmail.com>
2020-01-13 00:22:08 +0200

    read file outside war

--- src/main/java/service/ReadProperties.java
+++ src/main/java/service/ReadProperties.java
@@ -1,5 +1,7 @@
 package service;
 
+import controller.DbWorker;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,9 +10,20 @@ import java.util.Properties;
 public class ReadProperties {
 
     public static Properties readProperties() {
-        try (InputStream input = new FileInputStream("./config.cfg")) {
+
+        InputStream inputStream =
+   DbWorker.class.getResourceAsStream("db.properties");
+
+        try (InputStream input = new FileInputStream("db.properties")) {
             Properties properties = new Properties();
-            properties.load(input);
+            properties.load(inputStream);
+
+            System.out.println(properties.getProperty("jdbc.driver"));
+            System.out.println(properties.getProperty("db.url"));
+            System.out.println(properties.getProperty("db.name"));
+            System.out.println(properties.getProperty("db.user"));
+            System.out.println(properties.getProperty("db.password"));
+
             return properties;
         } catch (IOException ex) {
             ex.printStackTrace();
